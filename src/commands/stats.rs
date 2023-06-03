91234281772158f520dd1a50c9d2b8a9af569116
Ruthenic <mdrakea3@tutanota.com>
2023-06-03 15:01:26 -0400

    more accurate user count in /stats

--- src/commands/stats.rs
+++ src/commands/stats.rs
@@ -1,3 +1,5 @@
+use crate::models::DBMate;
+
 use super::CommandContext;
 use anyhow::{Context, Result};
 use mongodb::bson::doc;
@@ -6,11 +8,17 @@ use poise::serenity_prelude::CacheHttp;
 /// Get the statistics of the bot
 #[poise::command(slash_command)]
 pub async fn stats(ctx: CommandContext<'_>) -> Result<()> {
+    let database = &ctx.data().database;
+    let mates_collection = database.collection::<DBMate>("mates");
+
     let cache = ctx
         .cache()
         .context("Failed to get bot cache; try again later!")?;
 
-    let user_count = cache.user_count();
+    let user_count = mates_collection
+        .distinct("user_id", None, None)
+        .await?
+        .len();
     let guild_count = cache.guild_count();
 
     ctx.say(format!(
